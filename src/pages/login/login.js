64cf9e2adf14ac2fa6ec234d9ccf80ffe0603483
Syed Zeeshan Haider <haidezee@corp.contoursoftware.net>
2023-03-08 13:57:56 +0500

    add routes

--- src/pages/login/login.js
+++ src/pages/login/login.js
@@ -1,12 +1,24 @@
 import { Link } from 'react-router-dom' 
+import '../../pages/login/style.css'
 
 function NewEvent() {
   return (
-    <form method="post" action="/events">
-      <input type="text" name="title" />
-      <input type="text" name="description" />
-      <Link to='/'><button type="submit">Create</button></Link>
-    </form>
+    <div className="login-wrapper">
+      <h1>Please Log In</h1>
+      <form>
+        <label>
+          <p>Username</p>
+          <input type="text" />
+        </label>
+        <label>
+          <p>Password</p>
+          <input type="password" />
+        </label>
+        <div>
+        <Link to='/'><button type="submit">Create</button></Link>
+        </div>
+      </form>
+    </div>
   );
 }
 
